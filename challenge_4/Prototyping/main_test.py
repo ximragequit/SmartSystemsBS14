9be0933339bfee878461c1de973842e0db4b330a
lipaliga <l.paliga@stud.bhh.de>
2023-07-05 10:20:25 +0200

    updated main_test.py
    db zugriff hoffentlich optimiert

--- challenge_4/Prototyping/main_test.py
+++ challenge_4/Prototyping/main_test.py
@@ -37,6 +37,7 @@ db_pw = 'Testing1234'
 display_message = "HADAG raus!"
 actual_coordinates = "your mum"
 ferry_availability = True
+max_water_level = 10
 
 # Callback function for the on_connect event
 def on_connect(client, userdata, flags, rc):
@@ -93,18 +94,52 @@ def write_text(text):
     ser_display.write(text.encode())
     ser_display.write(b'\n')
 
+def get_db():
+    return psycopg2.connect(
+               host = db_host,
+               port = db_port,
+               database = db_database,
+               user = db_usr,
+               password = db_pw
+               )
+
+def db_get_next_pk_id(table):
+    db = get_db()
+    cursor = db.cursor()
+    table_id = table + "_id"
+    select_query = f"SELECT MAX({table_id}) FROM {table}"
+    cursor.execute(select_query)
+    table_id_row = cursor.fetchone()
+    cursor.close()
+    db.close()
+    if table_id_row[0] is not None:
+        return(table_id_row[0] + 1)
+    else:
+	    return 1
+
+def db_insert_data(table, data):
+
+    db = get_db()
+    print("Connected to the PostgreSQL database!")
+    cursor = db.cursor()
+
+    cursor.execute(f"SELECT * FROM {table} LIMIT 0")
+    colnames = [desc[0] for desc in cursor.description]
+    formatted_columns = '(' + ', '.join(colnames) + ')'
+    cursor.close()
+
+    # Needs a check if data has same amount of Contents as there are Columns in this table
+    cursor = db.cursor()
+    cursor.execute(f"INSERT INTO {table} {formatted_columns} VALUES {data};")
+    db.commit()
+    cursor.close()
+    print('Inserted',data,'into',table, formatted_columns)
+
+    db.close()
+
 def main():
     # main code
     # connect to Database
-    global connection
-    connection = psycopg2.connect(
-    host = db_host,
-    port = db_port,
-    database = db_database,
-    user = db_usr,
-    password = db_pw
-    )
-    print("Connected to the PostgreSQL database!")
 
     mqtt_thread = threading.Thread(target=start_mqtt_client)
     mqtt_thread.start()
@@ -112,20 +147,22 @@ def main():
     clear_display()
 
     while True:
+        time_now = time.time()
 
         # get waterlevel and publish
+        water_ID = db_get_next_pk_id("WaterLevel")
         water = ser_water.readline().decode('utf-8').strip()
-        message_water = str(water)
-        publish_message(mqtt_water, message_water)
+        water_data = (int(water_ID), str(time_now), float(water))
+        db_insert_data('WaterLevel',water_data)
+        #message_water = str(water)
+        #publish_message(mqtt_water, message_water)
 
-        if int(water) < 10: #ferry is available
+        if int(water) < max_water_level: #ferry is available
             ferry_availability = True
         else:
             ferry_availability = False
 
         publish_message(mqtt_availability, ferry_availability)
-
-        time_now = time.time()
 
         if ferry_availability:
             clear_display()
