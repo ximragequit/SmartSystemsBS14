601b484ba23220928234f8de16817467f1c382c0
lipaliga <l.paliga@stud.bhh.de>
2023-07-06 12:11:37 +0200

    log test

--- challenge_4/Prototyping/main_test.py
+++ challenge_4/Prototyping/main_test.py
@@ -7,7 +7,7 @@ import paho.mqtt.client as mqtt
 
 logging.basicConfig(
 	format = '______________________\n%(levelname)-2s %(asctime)s \n%(message)s',
-	filename = F'.\challenge_4\Prototyping\logs\{datetime.today().strftime("%y.%m.%d")}_py.log', 
+	filename = F'.{datetime.today().strftime("%y.%m.%d")}_py.log',
 	encoding = 'utf-8', 
 	level = logging.DEBUG,
 	datefmt='%y.%m.%d %H:%M:%S'
